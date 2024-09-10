ba649cc4ca55ec36239aee04109d548322135779
tanmay4269 <tanmay4269@gmail.com>
2024-09-10 13:48:39 +0530

    added dist train script

---
+++ dist-train.sh
@@ -0,0 +1,13 @@
+# On 104.171.200.62 (the master node)
+python3 -m torch.distributed.launch \
+--nproc_per_node=2 --nnodes=2 --node_rank=0 \
+--master_addr=104.171.200.62 --master_port=1234 \
+main.py \
+--backend=nccl --use_syn --batch_size=8192 --arch=resnet152
+
+# On 104.171.200.182 (the worker node)
+python3 -m torch.distributed.launch \
+--nproc_per_node=2 --nnodes=2 --node_rank=1 \
+--master_addr=104.171.200.62 --master_port=1234 \
+main.py \
+--backend=nccl --use_syn --batch_size=8192 --arch=resnet152
